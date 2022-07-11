d21c5686fc0359170f2855f169cd8aa555a4c621
IlyaKuz26 <ilyaprime26@yandex.ru>
2022-07-11 17:19:11 +0700

    efrsb data to csv

--- xml_processing.py
+++ xml_processing.py
@@ -2,6 +2,7 @@ from lxml import etree
 from glob import glob
 from tqdm import tqdm
 import csv
+import re
 
 
 def search_in_xml(dirpath):
@@ -12,9 +13,11 @@ def search_in_xml(dirpath):
         n = 0
         prefix_map = {"xsi": 'http://www.w3.org/2001/XMLSchema-instance'}
 
-        for year in glob(dirpath):
+        regex = re.compile(r".{4}-.{2}-.{2}")
 
-            for file in tqdm(glob(year), desc="Files"):
+        for year in glob(dirpath + '/*'):
+
+            for file in tqdm(glob(year+'/*.xml'), desc="Files"):
                 root = etree.parse(file).getroot()
 
                 for elem in root.findall(""".//Bankrupt[@xsi:type="Bankrupt.Company.v2"]""", prefix_map):
@@ -24,10 +27,11 @@ def search_in_xml(dirpath):
                     inn = inn_elem.text if inn_elem is not None else ''
                     publish_date_elem = elem.find('../PublishDate')
                     publish_date = publish_date_elem.text if publish_date_elem is not None else ''
+                    publish_date = regex.match(publish_date)[0]
 
                     writer.writerow([n, ogrn, inn, publish_date])
                     n += 1
 
 
 if __name__ == '__main__':
-    search_in_xml(r'D:\ЭкоТомск\practice3\пример xml.txt')
+    search_in_xml(r'\\192.168.100.50\usershare\dmitriy.chernykh@ecotomsk.com\crispdm\ЕФРСБ')
