d129e359583ec67540b53e65545acccfc34ae6c1
IlyaKuz26 <ilyaprime2@yandex.ru>
2022-07-11 14:39:12 +0700

    modify xml_processing

--- xml_processing.py
+++ xml_processing.py
@@ -1,8 +1,33 @@
 from lxml import etree
+from glob import glob
+from tqdm import tqdm
+import csv
 
-xml = r'\\192.168.100.50\usershare\dmitriy.chernykh@ecotomsk.com\crispdm\ЕФРСБ\2021\mes-6-2021_1.xml'
-tree = etree.parse(xml)
-root = tree.getroot()
 
-for elm in root.findall("Bankrupt"):
-    print(elm.tag)
+def search_in_xml(dirpath):
+    with open("efrsb.csv", 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(['№', 'Огрн', 'Инн', 'Дата банкротства'])
+
+        n = 0
+        prefix_map = {"xsi": 'http://www.w3.org/2001/XMLSchema-instance'}
+
+        for year in glob(dirpath):
+
+            for file in tqdm(glob(year), desc="Files"):
+                root = etree.parse(file).getroot()
+
+                for elem in root.findall(""".//Bankrupt[@xsi:type="Bankrupt.Company.v2"]""", prefix_map):
+                    ogrn_elem = elem.find('./Ogrn')
+                    ogrn = ogrn_elem.text if ogrn_elem is not None else ''
+                    inn_elem = elem.find('./Inn')
+                    inn = inn_elem.text if inn_elem is not None else ''
+                    publish_date_elem = elem.find('../PublishDate')
+                    publish_date = publish_date_elem.text if publish_date_elem is not None else ''
+
+                    writer.writerow([n, ogrn, inn, publish_date])
+                    n += 1
+
+
+if __name__ == '__main__':
+    search_in_xml(r'D:\ЭкоТомск\practice3\пример xml.txt')
